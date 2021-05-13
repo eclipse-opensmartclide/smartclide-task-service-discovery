b4a9000590d25ac8e74a7be22261992627ff763e
David B <40240397+dabm-git@users.noreply.github.com>
2021-05-13 14:32:48 +0200

    Update some files

--- Service code repositories/WIP_Github_search.py
+++ Service code repositories/WIP_Github_search.py
@@ -1,34 +1,96 @@
+import pandas as pd
+import time
+import calendar
+from datetime import datetime
 
 from github import Github
+from github import RateLimitExceededException
 
-ACCESS_TOKEN = 'Redacted' # Github persoal API
+ACCESS_TOKEN = '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'  # Github persoal API
 g = Github(ACCESS_TOKEN)
 
-# We look at the readme and the project description.
-def search_github_repos(keywords):
+# We look at the readme and the project description
 
-    # Split keywords
+
+def search_github_repos(keywords):
+    global g
+
+    # Split keywords in case of multiple
     keywords = [keyword.strip() for keyword in keywords.split(',')]
+
     # The query is set
     query = '+'.join(keywords) + '+in:readme+in:description'
+
     result = g.search_repositories(query, 'stars', 'desc')
- 
-    print(f'Found {result.totalCount} repo(s)')
- 
-    for repo in result:
-        print(f'{repo.clone_url}, {repo.stargazers_count} stars')
+
+    # Limited to 1k for search
+    print(f'Found {result.totalCount} repo(s) with the keywords',
+          ','.join(keywords))
+    df_temp = pd.DataFrame()
+    df_temp['Url'] = ""
+    df_temp['Description'] = ""
+    df_temp['Topics'] = ""
+    df_temp['Stars'] = ""
+
+    iter_obj = iter(result)
+    while True:
+
+        try:
+            for repo in result:
+                #count += 1
+                cloneUrl = str({repo.clone_url}).replace(
+                    "{'", "").replace("'}", "")
+                description = str({repo.description}).replace(
+                    "{'", "").replace("'}", "")
+                stars = str({repo.stargazers_count}).replace(
+                    "{", "").replace("}", "")
+                topics = ','.join(repo.get_topics())
+
+                df_repo = pd.DataFrame(
+                    {'Url': cloneUrl, 'Description': description, 'Topics': topics, 'Stars': stars}, index=[0])
+
+                df_temp = df_temp.append(df_repo)
+
+                # Dw readme?
+                #READMEcontents = repo.get_contents("readme.md")
+
+            df_temp = df_temp.reset_index(drop=True)
+            # One file per keyword
+            df_temp.to_csv(r'C:/Users/David/Desktop/Github/' + 'GitHub_' + ','.join(keywords) +
+                           '_' + datetime.now().strftime('%d_%m_%Y') + '.csv', index=True, header=True)
+            raise StopIteration
+
+        except StopIteration:
+            break
+
+        except RateLimitExceededException:
+            # Normalmente es 1h
+
+            print("\nRateLimitExceededException")
+            search_rate_limit = g.get_rate_limit().search
+            reset_timestamp = calendar.timegm(
+                search_rate_limit.reset.timetuple())
+            sleep_time = reset_timestamp - calendar.timegm(time.gmtime()) + 10
+            print("Sleeping (s): ", sleep_time * 1200)
+
+            time.sleep(3600)
+
+            g = Github(ACCESS_TOKEN)
+
+    return df_temp
 
 
 def search_github_files(keywords, extension):
 
-    # Split keywords	
+    # Split keywords
     keywords = [keyword.strip() for keyword in keywords.split(',')]
-    
+
     # The API call limit is checked.
     rate_limit = g.get_rate_limit()
     rate = rate_limit.search
     if rate.remaining == 0:
-        print(f'You have 0/{rate.limit} API calls remaining. Reset time: {rate.reset}')
+        print(
+            f'You have 0/{rate.limit} API calls remaining. Reset time: {rate.reset}')
         return
     else:
         print(f'You have {rate.remaining}/{rate.limit} API calls remaining')
@@ -38,8 +100,8 @@ def search_github_files(keywords, extension):
     # Search query
     result = g.search_code(query, order='desc')
 
-    # The list of repositories containing the keywords is printed out. 
-    # in the file extension extension
+    # The list of repositories containing the keywords is printed out
+    # in the file extension
     max_size = 100
     print(f'Found {result.totalCount} file(s)')
 
@@ -51,8 +113,17 @@ def search_github_files(keywords, extension):
     for file in result:
         print(f'{file.download_url}')
 
-# Example
-search_github_files("", ".wsdl")
-search_github_repos(".wsdl")
 
-# TODO> dataframes
+df_github = pd.DataFrame()
+
+fileKw = open("C:/Users/David/Desktop/keywordsAll.txt")
+for kw in fileKw:
+    print("Keyword: ", kw.rstrip('\n'))
+    df_temp = search_github_repos(kw)
+    df_github = df_github.append(df_temp).reset_index(drop=True)
+
+df_github.to_csv(r'C:/Users/David/Desktop/Github/' + 'GitHub_All_' +
+                 datetime.now().strftime('%d_%m_%Y') + '.csv', index=True, header=True)
+
+#search_github_files("", ".wsdl")
+# search_github_repos(keywords)
--- Service code repositories/apitest_bitbucket.py
+++ Service code repositories/apitest_bitbucket.py
@@ -1,118 +1,23 @@
-import requests, json, re, time, concurrent.futures, random
+import requests, json, re, time, concurrent.futures, random, threading
 import pandas as pd
 import numpy as np
 from datetime import datetime
 from bs4 import BeautifulSoup
 from config import config
 
-tokens = config.access_token_bitbucket_arr
-
-# Get info for repo url with api
-def getRepo(urlrepo, kw):
-    global dataglobal
-    global tokens
-    ctrl = True
-    url = "https://api.bitbucket.org/2.0/repositories" + urlrepo
-    token = tokens[int(random.random()*len(tokens))]
-    headers = {
-        'access_token': token
-    }
-    repo = requests.get(url,headers=headers)
-
-    # limit api requests
-    while repo.status_code==429:
-        tokens.remove(token)
-        print("Api limit error (429)")
-        if(len(tokens)==0):
-            time.sleep(3600)
-            tokens = config.access_token_bitbucket_arr
-        token = tokens[int(random.random()*len(tokens))]
-        headers = {
-            'access_token': token
-        }
-        repo = requests.get(url,headers=headers)
-
-    repo = repo.json()
-    while(ctrl):
-        repos = ""
-        #Search in the repository
-        try:
-            #Search README
-            for f in repo['values']:
-                    if('escaped_path' in f):
-                        if("README" in f['escaped_path']):
-                            text = requests.get(f['links']['self']['href']).text
-                            lines = text.split(" ")
-                            for line in lines:
-                                if("github" in line):
-                                    match = re.search(r'\w*github.com/\w*', line)
-                                    if(match is not None):
-                                        repos += match.group()
-                            ctrl = False
-
-            #Next page of files
-            if('next' in repo):
-                url = repo['next']
-            else:
-                ctrl = False
-        except KeyError:
-                ctrl = False
-    desc = " ".join(re.split("\s+", str(repo['description']), flags=re.UNICODE))
-    datarepo = {
-        "full_name": repo['full_name'],
-        "description": desc,
-        "type": repo['type'] ,
-        "link": repo['links']['clone'][0]['href'],
-        "link_github": repos ,
-        "language": repo['language'],
-        "keyword": kw
-    }
-    dataglobal.append(datarepo)
-    df = pd.json_normalize(data=dataglobal)
-    df.to_csv(r'C:/Datos/Repos/pruebas/docker/outputsB/output_bitbucket_'+kw.replace(" ","")+"_" + datetime.now().strftime('%d_%m_%Y') + '.csv', index = False)
-    return datarepo
-
-def getReposPage(i,kw):
-    # Get repos from the page
-    url = "https://bitbucket.org/repo/all/"+str(i)+"?name="+kw
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'lxml')
-    articles = soup.find_all("article", {"class": "repo-summary"})
-    repos = []
-    tasks = []
-    for article in articles:
-        for a in article.find_all('a', {"class": "avatar-link"}, href=True):
-
-            # a['href'] => ENLACE AL REPO
-            print(a['href'])
-            repos.append(a['href'])
-
-            with concurrent.futures.ThreadPoolExecutor(max_workers = 24) as executor:
-                tasks.append(executor.submit(getRepo, a['href'],kw))
-
-            # Iterate results
-            for result in tasks:
-                try:
-                    if(result.result()!="429"):
-                        data.append(result.result())
-
-
-                    # Limit api requests
-                    else:
-                        print("Error 429, omitiendo...")
-                        time.sleep(600)
-
-                except ValueError:  # includes simplejson.decoder.JSONDecodeError
-                    print("ValueError")
-
-    df = pd.json_normalize(data=data)
-    df.to_csv('output_bitbucket3.csv', index = False)
+tokens = config.access_token_bitbucket_arr.copy()
+mutex = threading.Lock()
+data403 = []
+data500 = []
+dataErr = []
 
 def getReposKw(kw):
+    global tokens
+    global data403
+    global data500
+    global dataErr
     print(kw)
-    cont = 1 # Page counter
-    base_url = "https://bitbucket.org/repo/all/"+str(cont)+"?name="+kw
-    tasks = []
+    base_url = "https://bitbucket.org/repo/all/1?name="+kw
 
     # Get max pages
     r = requests.get(base_url)
@@ -123,23 +28,136 @@ def getReposKw(kw):
     numbers = rx.findall(n2.text)
     limit = (int(numbers[0])//10)+1
 
+    data = []
     # Iterate all pages
-    for i in range(1, limit):
-        print(str(i) +" / "+str(limit))
-        with concurrent.futures.ThreadPoolExecutor(max_workers = 24) as executor:
-            tasks.append(executor.submit(getReposPage, i,kw))
+    for i in range(1, limit+1):
 
-f = open("keywords.txt")
+        url = "https://bitbucket.org/repo/all/"+str(i)+"?name="+kw
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'lxml')
+        articles = soup.find_all("article", {"class": "repo-summary"})
+        print(str(i) +" / "+str(limit) + " >> "+kw + " == "+str(len(articles))+" ")
+        for article in articles:
+            for a in article.find_all('a', {"class": "repo-link"}, href=True):
+                ctrl = True
+                url = "https://api.bitbucket.org/2.0/repositories" + a['href']
+
+                while(len(tokens)==0):
+                    time.sleep(10)
+
+                x = int(random.random()*len(tokens))
+                token = tokens[x]
+                headers = {
+                    'access_token': token
+                }
+                repo = requests.get(url,headers=headers)
+
+                # limit api requests
+                while repo.status_code==429:
+                    if(token in tokens):
+                        tokens.remove(token)
+                    print("Api limit error (429). Tokens restantes "+str(len(tokens)))
+                    mutex.acquire()
+                    if(len(tokens)==0):
+                        time.sleep(3600)
+                        tokens = config.access_token_bitbucket_arr.copy()
+                        print("Tokens reiniciados")
+                    mutex.release()
+                    token = tokens[int(random.random()*len(tokens))]
+                    headers = {
+                        'access_token': token
+                    }
+                    repo = requests.get(url,headers=headers)
+
+                if(repo.status_code==200):
+                    repo = repo.json()
+                    while(ctrl):
+                        repos = ""
+                        #Search in the repository
+                        try:
+                            #Search README
+                            for f in repo['values']:
+                                if('escaped_path' in f):
+                                    if("README" in f['escaped_path']):
+                                        text = requests.get(f['links']['self']['href']).text
+                                        lines = text.split(" ")
+                                        for line in lines:
+                                            if("github" in line):
+                                                match = re.search(r'\w*github.com/\w*', line)
+                                                if(match is not None):
+                                                    repos += match.group()
+                                        ctrl = False
+
+                            #Next page of files
+                            if('next' in repo): # ToDo Revisar bucle
+                                print("bucle pages")
+                                url = repo['next']
+                            else:
+                                ctrl = False
+                        except KeyError:
+                                ctrl = False
+                    desc = " ".join(re.split("\s+", str(repo['description']), flags=re.UNICODE))
+                    datarepo = {
+                        "full_name": repo['full_name'],
+                        "description": desc,
+                        "type": repo['type'] ,
+                        "link": repo['links']['clone'][0]['href'],
+                        "link_github": repos ,
+                        "language": repo['language'],
+                        "created_on": repo['created_on'],
+                        "updated_on": repo['updated_on'],
+                        "updated_on": repo['updated_on'],
+                        "keyword": kw
+                    }
+                    data.append(datarepo)
+                elif(repo.status_code==403):
+                    data403.append(a['href'])
+                elif(repo.status_code==500):
+                    data500.append(a['href'])
+                else:
+                    dataErr.append(a['href']+" => "+str(repo.status_code))
+
+        df = pd.json_normalize(data=data)
+        df.to_csv(r'C:/Datos/Repos/pruebas/docker/outputsB11M/output_bitbucket_'+kw.replace(" ","")+"_" + datetime.now().strftime('%d_%m_%Y') + '.csv', index = False)
+
+    return data
+
+f = open("keywordsAll.txt")
 keywords = []
 for line in f:
     keywords.append(line.rstrip('\n'))
 f.close()
 
-dataglobal = []
 data=[]
 tasks = []
-keywords_split  = np.array_split(keywords, 24) # max workers
-for split in range(len(keywords_split)):
-    with concurrent.futures.ThreadPoolExecutor(max_workers = len(keywords_split)) as executor:
-        for kw in keywords_split[split]:
-            tasks.append(executor.submit(getReposKw, kw))
+# Iterate keywordss
+keywords_split  = np.array_split(keywords, 25)
+with concurrent.futures.ThreadPoolExecutor(max_workers = len(keywords_split)) as executor:
+
+    for kw in keywords:
+        tasks.append(executor.submit(getReposKw, kw))
+
+# iterate results
+print("Tratando resultados")
+for result in tasks:
+    data+=result.result()
+
+print("Generando fichero")
+df = pd.json_normalize(data=data)
+df.to_csv('output_bitbucket11M.csv', index = False)
+print("Listo! ")
+
+print("Generando fichero403")
+df = pd.json_normalize(data=data403)
+df.to_csv('output_bitbucket11M403.csv', index = False)
+print("Listo! ")
+
+print("Generando fichero500")
+df = pd.json_normalize(data=data500)
+df.to_csv('output_bitbucket11M500.csv', index = False)
+print("Listo! ")
+
+print("Generando ficheroErr")
+df = pd.json_normalize(data=dataErr)
+df.to_csv('output_bitbucket11MErr.csv', index = False)
+print("Listo! ")
