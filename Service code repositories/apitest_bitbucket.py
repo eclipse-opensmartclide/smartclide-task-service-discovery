0d9ea89f6806b615c47ea1904bb7032b7badf8f3
David B <40240397+dabm-git@users.noreply.github.com>
2021-05-24 09:31:09 +0200

    Update apitest_bitbucket.py

--- Service code repositories/apitest_bitbucket.py
+++ Service code repositories/apitest_bitbucket.py
@@ -1,26 +1,72 @@
-import requests, json, re, time, concurrent.futures, random, threading
+import requests, json, re, time, concurrent.futures, threading, requests_random_user_agent
 import pandas as pd
 import numpy as np
-from datetime import datetime
-from bs4 import BeautifulSoup
 from config import config
+from bs4 import BeautifulSoup
+from datetime import datetime
 
-tokens = config.access_token_bitbucket_arr.copy()
-mutex = threading.Lock()
-data403 = []
-data500 = []
+# Initial vars
+data=[]
+tasks = []
 dataErr = []
+keywords = []
+del_columns=[
+    'connectActions',
+    'cloneProtocol',
+    'mirrors',
+    'menuItems',
+    'bitbucketActions',
+    'activeMenuItem',
+    'currentRepository.links.clone',
+    'currentRepository.links.self.href',
+    'currentRepository.links.avatar.href',
+    'currentRepository.project.links.self.href',
+    'currentRepository.project.links.avatar.href',
+    'currentRepository.project.owner.links.self.href',
+    'currentRepository.project.owner.links.avatar.href',
+    'currentRepository.project.workspace.links.self.href',
+    'currentRepository.project.workspace.links.avatar.href',
+    'currentRepository.project.is_private',
+    'currentRepository.owner.links.self.href',
+    'currentRepository.owner.links.avatar.href',
+    'currentRepository.owner.is_active',
+    'currentRepository.owner.department',
+    'currentRepository.owner.has_2fa_enabled',
+    'currentRepository.is_private',
+    'currentRepository.project.owner.uuid',
+    'currentRepository.project.workspace.uuid',
+]
+mutex = threading.Lock()
 
-def getReposKw(kw):
-    global tokens
-    global data403
-    global data500
+def getRequest(url):
+    """
+    Create request to param url
+
+    :param url: str
+    :return ResulSet:
+    """
+    headers = {
+        'access_token': config.access_token_bitbucket_arr_2
+    }
+    r = requests.get(url,headers=headers)
+    while(r.status_code==429): # If api limit, sleep 120s
+        print("["+str(datetime.now().strftime('%H:%M:%S'))+"] \tError 429 "+url)
+        time.sleep(120)
+        r = requests.get(url,headers=headers)
+    return r
+
+def getInfoReposFromKw(kw):
+    """
+    Create json list with all information from bitbucket search with keyword
+
+    :param kw: str
+    :return List:
+    """
     global dataErr
-    print(kw)
-    base_url = "https://bitbucket.org/repo/all/1?name="+kw
 
-    # Get max pages
-    r = requests.get(base_url)
+    # Start keyword & Get max pages
+    print("["+str(datetime.now().strftime('%H:%M:%S'))+"] \t"+kw +" >> \tEMPEZANDO ")
+    r = getRequest("https://bitbucket.org/repo/all/1?name="+kw)
     soup = BeautifulSoup(r.text, 'lxml')
     n = soup.find("section",{"class":"aui-item"})
     n2 = n.find("h1")
@@ -28,136 +74,88 @@ def getReposKw(kw):
     numbers = rx.findall(n2.text)
     limit = (int(numbers[0])//10)+1
 
-    data = []
     # Iterate all pages
+    data = []
     for i in range(1, limit+1):
 
-        url = "https://bitbucket.org/repo/all/"+str(i)+"?name="+kw
-        r = requests.get(url)
+        # Take information from i page
+        r = getRequest("https://bitbucket.org/repo/all/"+str(i)+"?name="+kw)
         soup = BeautifulSoup(r.text, 'lxml')
         articles = soup.find_all("article", {"class": "repo-summary"})
-        print(str(i) +" / "+str(limit) + " >> "+kw + " == "+str(len(articles))+" ")
+        print("["+str(datetime.now().strftime('%H:%M:%S'))+"] \t"+kw + " >> \t"+str(i) +" / "+str(limit) + " == "+str(len(articles))+" ")
+
+        # Iterate all repos in page
         for article in articles:
             for a in article.find_all('a', {"class": "repo-link"}, href=True):
-                ctrl = True
-                url = "https://api.bitbucket.org/2.0/repositories" + a['href']
+                repo = getRequest("https://bitbucket.org" + a['href'])
 
-                while(len(tokens)==0):
-                    time.sleep(10)
-
-                x = int(random.random()*len(tokens))
-                token = tokens[x]
-                headers = {
-                    'access_token': token
-                }
-                repo = requests.get(url,headers=headers)
-
-                # limit api requests
-                while repo.status_code==429:
-                    if(token in tokens):
-                        tokens.remove(token)
-                    print("Api limit error (429). Tokens restantes "+str(len(tokens)))
-                    mutex.acquire()
-                    if(len(tokens)==0):
-                        time.sleep(3600)
-                        tokens = config.access_token_bitbucket_arr.copy()
-                        print("Tokens reiniciados")
-                    mutex.release()
-                    token = tokens[int(random.random()*len(tokens))]
-                    headers = {
-                        'access_token': token
-                    }
-                    repo = requests.get(url,headers=headers)
-
+                # Append information repo to json list
                 if(repo.status_code==200):
-                    repo = repo.json()
-                    while(ctrl):
-                        repos = ""
-                        #Search in the repository
-                        try:
-                            #Search README
-                            for f in repo['values']:
-                                if('escaped_path' in f):
-                                    if("README" in f['escaped_path']):
-                                        text = requests.get(f['links']['self']['href']).text
-                                        lines = text.split(" ")
-                                        for line in lines:
-                                            if("github" in line):
-                                                match = re.search(r'\w*github.com/\w*', line)
-                                                if(match is not None):
-                                                    repos += match.group()
-                                        ctrl = False
-
-                            #Next page of files
-                            if('next' in repo): # ToDo Revisar bucle
-                                print("bucle pages")
-                                url = repo['next']
-                            else:
-                                ctrl = False
-                        except KeyError:
-                                ctrl = False
-                    desc = " ".join(re.split("\s+", str(repo['description']), flags=re.UNICODE))
-                    datarepo = {
-                        "full_name": repo['full_name'],
-                        "description": desc,
-                        "type": repo['type'] ,
-                        "link": repo['links']['clone'][0]['href'],
-                        "link_github": repos ,
-                        "language": repo['language'],
-                        "created_on": repo['created_on'],
-                        "updated_on": repo['updated_on'],
-                        "updated_on": repo['updated_on'],
-                        "keyword": kw
-                    }
-                    data.append(datarepo)
-                elif(repo.status_code==403):
-                    data403.append(a['href'])
-                elif(repo.status_code==500):
-                    data500.append(a['href'])
+                    repotext = repo.text.split('window.__initial_state__ = {"section": {"repository": ')
+                    if(len(repotext)>1):
+                        datajson = repotext[1].split('}, "global":')[0]
+                        datajson = " ".join(re.split(r"\s+", datajson, flags=re.UNICODE))
+                        datajson = " ".join(re.split(r"\\n+", datajson, flags=re.UNICODE))
+                        datajson = " ".join(re.split(r"\\t+", datajson, flags=re.UNICODE))
+                        datajson = " ".join(re.split(r"\\r+", datajson, flags=re.UNICODE))
+                        datajson = " ".join(re.split(r"\\r\\n+", datajson, flags=re.UNICODE))
+                        data.append(json.loads(datajson))
+                    else:
+                        dataErr.append({
+                            "link": a['href'],
+                            "error": "No info",
+                            "keyword": kw
+                        })
                 else:
-                    dataErr.append(a['href']+" => "+str(repo.status_code))
+                    dataErr.append({
+                        "link": a['href'],
+                        "error": str(repo.status_code),
+                        "keyword": kw
+                    })
 
-        df = pd.json_normalize(data=data)
-        df.to_csv(r'C:/Datos/Repos/pruebas/docker/outputsB11M/output_bitbucket_'+kw.replace(" ","")+"_" + datetime.now().strftime('%d_%m_%Y') + '.csv', index = False)
+        # Create security copy csv
+        generate_file('bitbucket_19M/bitbucket_'+kw.replace(" ","")+"_" + datetime.now().strftime('%d_%m_%Y') + '.csv',data,True)
 
+    # Return json list
     return data
 
-f = open("keywordsAll.txt")
-keywords = []
+def generate_file(name,data,delete):
+    """
+    Generate file with that configuration
+
+    :param name: str
+    :param data: List
+    :param delete: bool
+    :return List:
+    """
+    # Create dataframe from json list
+    print("["+str(datetime.now().strftime('%H:%M:%S'))+"] \tGenerando fichero"+name)
+    df = pd.json_normalize(data=data)
+    if(len(df.columns)>0 and delete):
+        # Delete some columns
+        df = df.drop(columns=del_columns,axis=1)
+    # Generate file
+    df.to_csv('outputs/'+name, index = False,encoding='utf-8-sig')
+    print("["+str(datetime.now().strftime('%H:%M:%S'))+"] \tListo! ")
+
+# Take all keywords from file
+f = open("keywords.txt")
 for line in f:
     keywords.append(line.rstrip('\n'))
 f.close()
 
-data=[]
-tasks = []
-# Iterate keywordss
-keywords_split  = np.array_split(keywords, 25)
+# Iterate keywords on thread
+keywords_split  = np.array_split(keywords, 10)
 with concurrent.futures.ThreadPoolExecutor(max_workers = len(keywords_split)) as executor:
-
     for kw in keywords:
-        tasks.append(executor.submit(getReposKw, kw))
+        tasks.append(executor.submit(getInfoReposFromKw, kw))
 
-# iterate results
-print("Tratando resultados")
+# Iterate results
+print("["+str(datetime.now().strftime('%H:%M:%S'))+"] \tTratando resultados")
 for result in tasks:
     data+=result.result()
 
-print("Generando fichero")
-df = pd.json_normalize(data=data)
-df.to_csv('output_bitbucket11M.csv', index = False)
-print("Listo! ")
-
-print("Generando fichero403")
-df = pd.json_normalize(data=data403)
-df.to_csv('output_bitbucket11M403.csv', index = False)
-print("Listo! ")
-
-print("Generando fichero500")
-df = pd.json_normalize(data=data500)
-df.to_csv('output_bitbucket11M500.csv', index = False)
-print("Listo! ")
-
-print("Generando ficheroErr")
-df = pd.json_normalize(data=dataErr)
-df.to_csv('output_bitbucket11MErr.csv', index = False)
-print("Listo! ")
+# Generate data file & error file
+generate_file('bitbucket2_'+ datetime.now().strftime('%d_%m') + '.csv',data,True)
+if(dataErr):
+    generate_file('bitbucket2_'+ datetime.now().strftime('%d_%m') + '_err.csv',dataErr,False)
